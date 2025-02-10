b7ee5a4512cd876ea1e3819be130b48546a38dc5
Kevin Torres <79941554+KevinTorres74@users.noreply.github.com>
2025-02-10 14:52:08 -0600

    Create Problema1.java

---
+++ Problema1.java
@@ -0,0 +1,19 @@
+public class Problema1 {
+    public static void main(String[] args) {
+        String dino;
+        dino = 
+        "               Dinosaurio\n" +
+            "                        . - ~ ~ ~ - .\n" +
+            "      ..     _      .-~               ~-.\n" +
+            "     //|     \\ `..~                      `.\n" +
+            "    || |      }  }              /       \\  \\\n" +
+            "(\\   \\\\ \\~^..'                 |         }  \\\n" +
+            " \\`.-~  o      /       }       |        /    \\\n" +
+            " (__          |       /        |       /      `.\n" +
+            "  `- - ~ ~ -._|      /_ - ~ ~ ^|      /- _      `.\n" +
+            "              |     /          |     /     ~-.     ~- _\n" +
+            "              |_____|          |_____|         ~ - . _ _~_-_\n"
+        ;
+        System.out.println(dino);
+    }
+}
